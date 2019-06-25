bd9b4424d04257b217b9813c10f09273b32075d6
Nick Brooks <nbrooks@thirtyseven.com.au>
2019-06-25 12:15:50 -0600

    testing headers

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
-if (module.hot) {
-  module.hot.accept();
-}
+const req = new XMLHttpRequest();
+req.open("GET", document.location, false);
+req.send(null);
+const headers = req.getAllResponseHeaders().toLowerCase();
+alert(headers);
